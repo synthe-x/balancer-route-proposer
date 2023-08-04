4b7a836100ea0affd4d6342e1ebc48bc1e22f850
mubashirzebi <mubashshirzebi@gmail.com>
2023-08-04 14:43:00 +0530

    remove rate limiter

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
 import morgan from "morgan";
-import rateLimit from 'express-rate-limit'
 import { startUpdatePrice } from './controller/route-proposer-handler/helper/fetch-price/token-prices';
 import { fetchSynthPoolData } from './controller/route-proposer-handler/handler/synth-pool/get-synth-details';
 import { fetchPoolData } from './controller/route-proposer-handler/handler/balancer-pool/fetch-data';
@@ -21,14 +20,6 @@ app.use(helmet());
 app.use(morgan("tiny"));
 app.use(compression());
 
-
-const limiter = rateLimit({
-    windowMs: 60 * 1000,
-    max: 80,
-    standardHeaders: true,
-    legacyHeaders: false,
-})
-app.use(limiter)
 app.use("/", route)
 fetchSynthPoolData()
 fetchPoolData()
